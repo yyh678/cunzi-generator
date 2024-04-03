2fb339d1f0b352b815ce643b50adc9c8f1bb6171
yyh678 <1789065028@qq.com>
2024-04-03 22:28:27 +0800

    新增jar包运行方式 第一阶段代码生产已全部完成

--- cunzi-generator-basic/src/main/java/com/cunnan/Main.java
+++ cunzi-generator-basic/src/main/java/com/cunnan/Main.java
@@ -9,9 +9,9 @@ import com.cunnan.cli.CommandExecutor;
 public class Main {
     public static void main(String[] args) {
 
-        args = new String[]{"generate", "-l", "-a", "-o"};
+        //args = new String[]{"generate", "-l", "-a", "-o"};
         // args = new String[]{"config"};
-        // args = new String[]{"list"};
+        args = new String[]{"list"};
         CommandExecutor commandExecutor = new CommandExecutor();
         commandExecutor.doExecute(args);
     }
